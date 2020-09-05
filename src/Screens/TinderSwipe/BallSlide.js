d369e23af51320749cb945e364d2720930aed22e
Yashas-N-Hashedin <yashas.n@hashedin.com>
2020-09-05 17:59:02 +0530

    Ball slide screen completed

--- src/Screens/TinderSwipe/BallSlide.js
+++ src/Screens/TinderSwipe/BallSlide.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { View, StyleSheet, SafeAreaView } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import {
+  View,
+  StyleSheet,
+  Animated,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import { Ball } from '../../Components/Ball';
 
 export const BallSlide = () => {
+  const position = new Animated.ValueXY(0, 0);
+  const moveBall = () => {
+    let x = Math.floor(Math.random() * 300);
+    let y = Math.floor(Math.random() * 500);
+    Animated.spring(position, {
+      toValue: { x, y },
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const message = 'MOVE THE BALL';
+
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Ball />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={{ flexGrow: 1 }}>
+        <Animated.View style={position.getLayout()}>
+          <Ball />
+        </Animated.View>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => moveBall()}>
+        <Text style={styles.buttonText}>{message}</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -16,7 +39,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
+  },
+  button: {
+    width: '100%',
+    height: '10%',
+    backgroundColor: 'black',
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    width: '100%',
+    textAlign: 'center',
   },
 });
